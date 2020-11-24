f2ba980ae6ca7c749cb08436c2d9679f11ace45e
Nolan VanNurden <nolanvannurden@Nolans-MacBook-Pro.local>
2020-11-24 16:09:00 -0700

    11/24 @ 4:08

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import Auth from './components/Auth';
+import Auth from './components/Auth/Auth';
 import Post from './components/Post';
 import Feed from './components/Feed';
 export default (
